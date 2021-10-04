2da0bd557d7b34f28cfaa62052af3ffd3e9fd200
unknown <yisehakmitiku@gmail.com>
2021-10-04 21:48:22 +0800

    Fix linter errors

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const hamburger = document.querySelector('.fa-bars');
 const menu = document.querySelector('#menu');
-const cancel_icon = document.querySelector('.cancel-icon');
+const cancelIcon = document.querySelector('.cancel-icon');
 
-
-hamburger.addEventListener('click', (e) => {
+hamburger.addEventListener('click', () => {
   menu.classList.replace('invisible', 'menu');
-
 });
 
-cancel_icon.addEventListener('click', (e) => {
+cancelIcon.addEventListener('click', () => {
   menu.classList.replace('menu', 'invisible');
-
 });
